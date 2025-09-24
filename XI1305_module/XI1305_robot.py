ce164303aafb1ec5ab8a8598f12a613d94a6afeb
Judman36 <hamishjudson36@gmail.com>
2025-09-24 17:21:05 +1000

    HamishArmDHIncorrect

---
+++ XI1305_module/XI1305_robot.py
@@ -0,0 +1,55 @@
+import swift
+import roboticstoolbox as rtb
+import spatialmath.base as spb
+import numpy as np
+from spatialmath import SE3
+from ir_support.robots.DHRobot3D import DHRobot3D
+import time
+import os
+
+# Useful variables
+from math import pi
+
+class XI1305(DHRobot3D):
+    def __init__(self):
+        # DH links
+        links = self._create_DH()
+
+        # Names of the robot link files in the directory
+        link3D_names = dict(link0 = 'XI1305_1', color0 = (0.2,0.2,0.2,1),      # color option only takes effect for stl file
+                            link1 = 'XI1305_2', color1=(0.5,0,0,1),
+                            link2 = 'XI1305_3',
+                            link3 = 'XI1305_4',
+                            link4 = 'XI1305_5',
+                            link5 = 'XI1305_6',
+                            link6 = 'XI1305_7')
+
+        # A joint config and the 3D object transforms to match that config
+        qtest = [0,0,0,pi,0,0]
+        qtest_transforms = [spb.transl(0,0,0),
+                            spb.transl(0,0.147,0),
+                            spb.transl(-0.06,0.265,0),
+                            spb.transl(-0.043,0.550,-0.053),
+                            spb.transl(0.003,0.3895,-0.1308),
+                            spb.transl(0.015,0.2095,-0.1308),
+                            spb.transl(0,0.1443,-0.206) @ spb.trotz(0.56723)]
+
+        current_path = os.path.abspath(os.path.dirname(__file__))
+        super().__init__(links, link3D_names, name = 'XI1305', link3d_dir = current_path, qtest = qtest, qtest_transforms = qtest_transforms)
+        self.base = self.base * SE3.Rx(pi/2) * SE3.Ry(pi/2)
+        self.q = qtest
+
+    def _create_DH(self):
+            """
+            Create robot's standard DH model
+            """
+            links = [] 
+            a = [0,0,-0.053,-0.0778,0,-0.0752]
+            d = [0.147,0.118,0.285,0.1605,0.18,0.0652]     # Static UR3: [0.1519, 0, 0, 0.11235, 0.08535, 0.0819]
+            alpha = [-pi/2,0,-pi/2,pi/2,-pi/2,0]
+            offset = [0,-1.3849,1.3849,0,0,0]
+            qlim = [[-2*pi, 2*pi] for _ in range(6)]
+            for i in range(6):
+                link = rtb.RevoluteDH(d=d[i], a=a[i], alpha=alpha[i], offset=offset[i], qlim=qlim[i])
+                links.append(link)
+            return links
