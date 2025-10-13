a17da1b2097decbfd8ae4a4d80dc44618bdee61e
Abhi <abhi.naglapura1@gmail.com>
2025-10-13 11:29:53 +1100

    Updated start and end position mappings for the new env height

--- Environment_Meshes/brick_data.py
+++ Environment_Meshes/brick_data.py
@@ -34,71 +34,70 @@ color_map = {
 }
 
 position_map_end = {
-    "scaled_9551.stl"          : (0.04,     0.08,       0.025),     #WHITE                     
-    "scaled_241223(1).stl"     : (0.28,     0.08,       0.025),     #EARTH BLUE                
-    "scaled_241223(2).stl"     : (0.44,     0.08,       0.089),     #EARTH BLUE                
-    "scaled_303701.stl"        : (0.32,     0.08,       0.057),     #WHITE                     
-    "scaled_4211065.stl"       : (0.48,     0.08,       0.121),     #BLACK                     
-    "scaled_4548180.stl"       : (-0.12,    0.08,       0.153),     #BLACK                     
-    "scaled_4632100(1).stl"    : (0.34627,  0.08,       0.15527),   #COOL SILVER DRUM LACQUERED
-    "scaled_4632100(2).stl"    : (0.42627,  0.08,       0.15527),   #COOL SILVER DRUM LACQUERED
-    "scaled_4632100(3).stl"    : (0.50627,  0.08,       0.15527),   #COOL SILVER DRUM LACQUERED
-    "scaled_6029208(1).stl"    : (-0.16,    -0.131,     0.),        #BLACK                     
-    "scaled_6029208(2).stl"    : (0.48,     -0.131,     0.),        #BLACK                     
-    "scaled_6029208(3).stl"    : (-0.16,    0.291,      0.),        #BLACK                     
-    "scaled_6029208(4).stl"    : (0.48,     0.291,      0.),        #BLACK                     
-    "scaled_6058966.stl"       : (0.6,      0.08,       0.225),     #BLACK                     
-    "scaled_6061047(1).stl"    : (0.08,     0.2,        0.057),     #WHITE                     
-    "scaled_6061047(2).stl"    : (0.08,     -0.03975,   0.057),     #WHITE                     
-    "scaled_6170524.stl"       : (-0.4,     0.08,       0.025),     #WHITE                     
-    "scaled_6186974.stl"       : (0.44,     0.08,       0.153),     #EARTH BLUE                
-    "scaled_6218226(1).stl"    : (-0.16,    -0.131,     0.030),     #COOL SILVER DRUM LACQUERED
-    "scaled_6218226(2).stl"    : (-0.16,    0.291,      0.030),     #COOL SILVER DRUM LACQUERED
-    "scaled_6218226(3).stl"    : (0.48,     -0.131,     0.030),     #COOL SILVER DRUM LACQUERED
-    "scaled_6218226(4).stl"    : (0.48,     0.291,      0.030),     #COOL SILVER DRUM LACQUERED
-    "scaled_6248833.stl"       : (0.48,     0.08,       0.057),     #TR. YELLOW                
-    "scaled_6251290.stl"       : (0.56,     0.08,       0.089),     #NEW DARK RED              
-    "scaled_6285534.stl"       : (0.16,     0.08,       0.025),     #BLACK                     
-    "scaled_6329585.stl"       : (-0.04,    0.08,       0.153),     #WHITE                     
-    "scaled_6365907(1).stl"    : (0.16,     -0.12,      0.057),     #BRIGHT ORANGE             
-    "scaled_6365907(2).stl"    : (0.16,     0.28,       0.057),     #BRIGHT ORANGE             
-    "scaled_6365908.stl"       : (-0.08,    0.08,       0.057),     #BRIGHT ORANGE             
-    "scaled_6370526.stl"       : (0.6,      0.08,       0.216)      #BRIGHT ORANGE             
+    "scaled_9551.stl"          : (-0.1192,    0.0008,     0.075),     #WHITE                     
+    "scaled_241223(1).stl"     : (0.1208,     0.0008,     0.075),     #EARTH BLUE                
+    "scaled_241223(2).stl"     : (0.2808,     0.0008,     0.139),     #EARTH BLUE                
+    "scaled_303701.stl"        : (0.1608,     0.0008,     0.107),     #WHITE                     
+    "scaled_4211065.stl"       : (0.3208,     0.0008,     0.171),     #BLACK                     
+    "scaled_4548180.stl"       : (-0.2792,    0.0008,     0.203),     #BLACK                     
+    "scaled_4632100(1).stl"    : (0.18707,    0.0008,     0.20527),   #COOL SILVER DRUM LACQUERED
+    "scaled_4632100(2).stl"    : (0.26707,    0.0008,     0.20527),   #COOL SILVER DRUM LACQUERED
+    "scaled_4632100(3).stl"    : (0.34707,    0.0008,     0.20527),   #COOL SILVER DRUM LACQUERED
+    "scaled_6029208(1).stl"    : (-0.3192,   -0.2102,     0.05),      #BLACK                     
+    "scaled_6029208(2).stl"    : (0.3208,    -0.2102,     0.05),      #BLACK                     
+    "scaled_6029208(3).stl"    : (-0.3192,    0.2118,     0.05),      #BLACK                     
+    "scaled_6029208(4).stl"    : (0.3208,     0.2118,     0.05),      #BLACK                     
+    "scaled_6058966.stl"       : (0.4408,     0.0008,     0.275),     #BLACK                     
+    "scaled_6061047(1).stl"    : (-0.0792,    0.1208,     0.107),     #WHITE                     
+    "scaled_6061047(2).stl"    : (-0.0792,   -0.11895,    0.107),     #WHITE                     
+    "scaled_6170524.stl"       : (-0.5592,    0.0008,     0.075),     #WHITE                     
+    "scaled_6186974.stl"       : (0.2808,     0.0008,     0.203),     #EARTH BLUE                
+    "scaled_6218226(1).stl"    : (-0.3192,   -0.2102,     0.08),      #COOL SILVER DRUM LACQUERED
+    "scaled_6218226(2).stl"    : (-0.3192,    0.2118,     0.08),      #COOL SILVER DRUM LACQUERED
+    "scaled_6218226(3).stl"    : (0.3208,    -0.2102,     0.08),      #COOL SILVER DRUM LACQUERED
+    "scaled_6218226(4).stl"    : (0.3208,     0.2118,     0.08),      #COOL SILVER DRUM LACQUERED
+    "scaled_6248833.stl"       : (0.3208,     0.0008,     0.107),     #TR. YELLOW                
+    "scaled_6251290.stl"       : (0.4008,     0.0008,     0.139),     #NEW DARK RED              
+    "scaled_6285534.stl"       : (0.0008,     0.0008,     0.075),     #BLACK                     
+    "scaled_6329585.stl"       : (-0.1992,    0.0008,     0.203),     #WHITE                     
+    "scaled_6365907(1).stl"    : (0.0008,    -0.1992,     0.107),     #BRIGHT ORANGE             
+    "scaled_6365907(2).stl"    : (0.0008,     0.2008,     0.107),     #BRIGHT ORANGE             
+    "scaled_6365908.stl"       : (-0.2392,    0.0008,     0.107),     #BRIGHT ORANGE             
+    "scaled_6370526.stl"       : (0.4408,     0.0008,     0.266)      #BRIGHT ORANGE             
 }
 
 position_map_start = {
-    "scaled_9551.stl"          : (0.5, 0.5, 0.0),       #WHITE                     
-    "scaled_241223(1).stl"     : (1.5, 0.0, 0.0),       #EARTH BLUE                
-    "scaled_241223(2).stl"     : (2.0, 0.0, 0.0),       #EARTH BLUE                
-    "scaled_303701.stl"        : (0.0, 0.5, 0.0),       #WHITE                     
-    "scaled_4211065.stl"       : (2.5, 0.0, 0.0),       #BLACK                     
-    "scaled_4548180.stl"       : (1.0, 1.5, 0.0),       #BLACK                     
-    "scaled_4632100(1).stl"    : (1.5, 1.0, 0.0),       #COOL SILVER DRUM LACQUERED
-    "scaled_4632100(2).stl"    : (2.0, 1.0, 0.0),       #COOL SILVER DRUM LACQUERED
-    "scaled_4632100(3).stl"    : (2.5, 1.0, 0.0),       #COOL SILVER DRUM LACQUERED
-    "scaled_6029208(1).stl"    : (1.0, 2.0, 0.0),       #BLACK                     
-    "scaled_6029208(2).stl"    : (1.5, 2.0, 0.0),       #BLACK                     
-    "scaled_6029208(3).stl"    : (2.0, 2.0, 0.0),       #BLACK                     
-    "scaled_6029208(4).stl"    : (2.5, 2.0, 0.0),       #BLACK                     
-    "scaled_6058966.stl"       : (0.5, 1.5, 0.0),       #BLACK                     
-    "scaled_6061047(1).stl"    : (1.0, 0.5, 0.0),       #WHITE                     
-    "scaled_6061047(2).stl"    : (1.5, 0.5, 0.0),       #WHITE                     
-    "scaled_6170524.stl"       : (0.5, 1.0, 0.0),       #WHITE                     
-    "scaled_6186974.stl"       : (1.0, 1.0, 0.0),       #EARTH BLUE                
-    "scaled_6218226(1).stl"    : (2.0, 1.5, 0.0),       #COOL SILVER DRUM LACQUERED
-    "scaled_6218226(2).stl"    : (2.5, 1.5, 0.0),       #COOL SILVER DRUM LACQUERED
-    "scaled_6218226(3).stl"    : (0.0, 2.0, 0.0),       #COOL SILVER DRUM LACQUERED
-    "scaled_6218226(4).stl"    : (0.5, 2.0, 0.0),       #COOL SILVER DRUM LACQUERED
-    "scaled_6248833.stl"       : (0.5, 0.0, 0.0),       #TR. YELLOW                
-    "scaled_6251290.stl"       : (1.0, 0.0, 0.0),       #NEW DARK RED              
-    "scaled_6285534.stl"       : (0.0, 0.0, 0.0),       #BLACK                     
-    "scaled_6329585.stl"       : (1.5, 1.5, 0.0),       #WHITE                     
-    "scaled_6365907(1).stl"    : (2.0, 0.5, 0.0),       #BRIGHT ORANGE             
-    "scaled_6365907(2).stl"    : (2.5, 0.5, 0.0),       #BRIGHT ORANGE             
-    "scaled_6365908.stl"       : (0.0, 1.0, 0.0),       #BRIGHT ORANGE             
-    "scaled_6370526.stl"       : (0.0, 1.5, 0.0)        #BRIGHT ORANGE             
+    "scaled_9551.stl"          : (0.5, 0.5, 0.05),       #WHITE                     
+    "scaled_241223(1).stl"     : (1.5, 0.0, 0.05),       #EARTH BLUE                
+    "scaled_241223(2).stl"     : (2.0, 0.0, 0.05),       #EARTH BLUE                
+    "scaled_303701.stl"        : (0.0, 0.5, 0.05),       #WHITE                     
+    "scaled_4211065.stl"       : (2.5, 0.0, 0.05),       #BLACK                     
+    "scaled_4548180.stl"       : (1.0, 1.5, 0.05),       #BLACK                     
+    "scaled_4632100(1).stl"    : (1.5, 1.0, 0.05),       #COOL SILVER DRUM LACQUERED
+    "scaled_4632100(2).stl"    : (2.0, 1.0, 0.05),       #COOL SILVER DRUM LACQUERED
+    "scaled_4632100(3).stl"    : (2.5, 1.0, 0.05),       #COOL SILVER DRUM LACQUERED
+    "scaled_6029208(1).stl"    : (1.0, 2.0, 0.05),       #BLACK                     
+    "scaled_6029208(2).stl"    : (1.5, 2.0, 0.05),       #BLACK                     
+    "scaled_6029208(3).stl"    : (2.0, 2.0, 0.05),       #BLACK                     
+    "scaled_6029208(4).stl"    : (2.5, 2.0, 0.05),       #BLACK                     
+    "scaled_6058966.stl"       : (0.5, 1.5, 0.05),       #BLACK                     
+    "scaled_6061047(1).stl"    : (1.0, 0.5, 0.05),       #WHITE                     
+    "scaled_6061047(2).stl"    : (1.5, 0.5, 0.05),       #WHITE                     
+    "scaled_6170524.stl"       : (0.5, 1.0, 0.05),       #WHITE                     
+    "scaled_6186974.stl"       : (1.0, 1.0, 0.05),       #EARTH BLUE                
+    "scaled_6218226(1).stl"    : (2.0, 1.5, 0.05),       #COOL SILVER DRUM LACQUERED
+    "scaled_6218226(2).stl"    : (2.5, 1.5, 0.05),       #COOL SILVER DRUM LACQUERED
+    "scaled_6218226(3).stl"    : (0.0, 2.0, 0.05),       #COOL SILVER DRUM LACQUERED
+    "scaled_6218226(4).stl"    : (0.5, 2.0, 0.05),       #COOL SILVER DRUM LACQUERED
+    "scaled_6248833.stl"       : (0.5, 0.0, 0.05),       #TR. YELLOW                
+    "scaled_6251290.stl"       : (1.0, 0.0, 0.05),       #NEW DARK RED              
+    "scaled_6285534.stl"       : (0.0, 0.0, 0.05),       #BLACK                     
+    "scaled_6329585.stl"       : (1.5, 1.5, 0.05),       #WHITE                     
+    "scaled_6365907(1).stl"    : (2.0, 0.5, 0.05),       #BRIGHT ORANGE             
+    "scaled_6365907(2).stl"    : (2.5, 0.5, 0.05),       #BRIGHT ORANGE             
+    "scaled_6365908.stl"       : (0.0, 1.0, 0.05),       #BRIGHT ORANGE             
+    "scaled_6370526.stl"       : (0.0, 1.5, 0.05)        #BRIGHT ORANGE             
 }
-
 
 rotation_map_end = {
     "scaled_9551.stl"          : (0,        0,       0),        #WHITE                     
